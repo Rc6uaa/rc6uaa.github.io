cf3ba9930ef4d26d78460db6e5f1054610c9e7d0
Rc6uaa <sergeytmm@mail.ru>
2025-01-26 17:52:29 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function showMessage() {
-    document.getElementById('message').innerText = 'Нейросети — это будущее! И ваше прошлое';
+    document.getElementById('message').innerText = 'Нейросети — это будущее!';
+}
+
+// Функции для открытия страниц
+function openMailRu() {
+    window.open('https://mail.ru', '_blank');
+}
+
+function openYaRu() {
+    window.open('https://ya.ru', '_blank');
+}
+
+function openAvitoRu() {
+    window.open('https://avito.ru', '_blank');
+}
+
+function openWildberries() {
+    window.open('https://www.wildberries.ru/', '_blank');
+}
+
+function openOzon() {
+    window.open('https://ozon.ru', '_blank');
 }
